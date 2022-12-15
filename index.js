859159896bc476950544822697ce148a88070837
nanda.r <geeknanda13@gmail.com>
2022-12-15 17:35:08 +0530

    updated

---
+++ index.js
@@ -0,0 +1,18 @@
+import { request, GraphQLClient } from 'graphql-request'
+
+
+let endpoint = 'https://countries.trevorblades.com/'
+
+let query = `query {
+  continents {
+    name
+  }
+  
+}`
+
+// Run GraphQL queries/mutations using a static function
+request(endpoint, query).then((data) => console.log(data))
+
+// ... or create a GraphQL client instance to send requests
+// const client = new GraphQLClient(endpoint, { headers: {} })
+// client.request(query, variables).then((data) => console.log(data))
